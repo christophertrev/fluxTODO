a6244f454b4e41572928bf2148aa6fcedd45fdfb
Christopher Treviño <christophertrev@gmail.com>
2015-02-03 21:25:11 -0800

    (feat) add Emotion store

--- src/stores/EmotionsStore.js
+++ src/stores/EmotionsStore.js
@@ -1,8 +1,17 @@
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+
+
+var _emotions = {};
+
 
 var EmotionsStore = assign({}, EventEmitter.prototype, {
+  
 
+  getAll: function() {
+    return _emotions;
+  },
 
 
 });
