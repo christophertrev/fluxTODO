95d8bbaecb35195daf69ed6815a5572f6d7711d1
Christopher Treviño <christophertrev@gmail.com>
2015-02-03 16:59:13 -0800

    (refactor) Pulled out js task to separate file

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,8 @@
-var gutil = require('gulp-util')
-var watchify = require('watchify');
-var browserify = require('browserify');
-var reactify = require('reactify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
- 
-
-
 var gulp = require('./gulp')([
-  'build'
+  'build',
+  'js'
 ]);
 
-
-
-
-var bundler = watchify(browserify('./src/index.js', watchify.args));
-// add any other browserify options or transforms here
-bundler.transform(reactify);
-
-gulp.task('js', bundle); // so you can run `gulp js` to build the file
-bundler.on('update', bundle); // on any dep update, runs the bundler
-
-function bundle() {
-  return bundler.bundle()
-    // log errors if they happen
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./lib'));
-}
 
 gulp.task('watch', function (){
   gulp.watch('src/*', ['build']);
