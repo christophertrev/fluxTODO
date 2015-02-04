94298a1973a735dd27fec1c6eff88c7cf074c728
Christopher Treviño <christophertrev@gmail.com>
2015-02-04 11:51:18 -0800

    (feat) Add Dummy emotion data

--- src/stores/EmotionsStore.js
+++ src/stores/EmotionsStore.js
@@ -3,16 +3,17 @@ var assign = require('object-assign');
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 
 
-var _emotions = {};
+var _emotions = {
+  1: {emotion: 'happy', description:'Having a great Day'},
+  2: {emotion:'sad', description:'Day could be going better'}
+};
 
 
 var EmotionsStore = assign({}, EventEmitter.prototype, {
   
-
   getAll: function() {
     return _emotions;
   },
-
 
 });
 
