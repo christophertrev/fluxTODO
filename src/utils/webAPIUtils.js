07114cb5efa5c428cd5c6165a9ac0e855defe099
Christopher Treviño <christophertrev@gmail.com>
2015-02-05 10:03:10 -0800

    (feat) add style sheet

--- src/utils/webAPIUtils.js
+++ src/utils/webAPIUtils.js
@@ -4,8 +4,8 @@ module.exports = {
   getAllEmotions: function (){
     console.log('getting all emotions')
     $.ajax({
-      // url: "http://emotifaces.herokuapp.com/emotion/"
-      url: "http://localhost:3000/emotion"
+      url: "http://emotifaces.herokuapp.com/emotion/"
+      // url: "http://localhost:3000/emotion"
     })
       .done(function( emotions ) {
         // if ( console && console.log ) {
